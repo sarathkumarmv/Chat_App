bb6cee905c64a7588363a8967ad8747665cee600
Sarath Kumar M V <sarathkumarmv1@gmail.com>
2023-03-08 16:04:14 +0530

    Add files via upload

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:809092841877:web:6d2c0d3e0d385f8f62b69c";
+  static String apiKey = "AIzaSyCC-HkKz6-orrkAvoQrcmV6PZ82wbAdGxs";
+  static String messagingSenderId = "809092841877";
+  static String projectId = "chatapp-b767e";
+  final primaryColor = const Color(0xFFee7b64);
+}
